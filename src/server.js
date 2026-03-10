cca76bec4fd60e69bedd2c89fb754c5c2425b8ac
ayushisahu19 <ayushisahu0512@gmail.com>
2026-03-10 18:24:19 +0530

    fix API port to 5000

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
