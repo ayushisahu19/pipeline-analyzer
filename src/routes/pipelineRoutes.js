bf308c77bd56e5ef26c4fb879932d8aa85f0784b
ayushisahu19 <ayushisahu0512@gmail.com>
2026-03-10 15:48:25 +0530

    Modification in pipelineRoutes.js

--- src/routes/pipelineRoutes.js
+++ src/routes/pipelineRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/pipelineController");
 
 router.post("/pipeline", controller.createRun);
+
 router.get("/pipeline", controller.getRuns);
+
 router.get("/pipeline/summary/:branch", controller.getBranchSummary);
 
 module.exports = router;
